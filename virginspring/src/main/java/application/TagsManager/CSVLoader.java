9dbc0013820fbcdf5c65820c92bea3efbfb72b06
Bailin Huang <bailin.huang@ucr.ac.cr>
2018-05-17 21:03:45 -0600

    Events

--- virginspring/src/main/java/application/TagsManager/CSVLoader.java
+++ virginspring/src/main/java/application/TagsManager/CSVLoader.java
@@ -1,12 +1,35 @@
-package application.TagsManager;
+package application.EventsManager;
+import jdk.internal.util.xml.impl.Input;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.*;
+import java.net.URL;
 import java.util.Scanner;
 
 public class CSVLoader {
+    @Autowired
+    ResourceLoader resourceLoader;
 
+    public CSVLoader() {
+
+    }
+
+    public String load(InputStream inputStream) {
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        String s = "what";
+        try(BufferedReader br = new BufferedReader(inputStreamReader)){
+            br.readLine();
+            br.readLine();
+            s = br.readLine();
+        } catch (IOException E){
+
+        }
+        return s;
+    }
+
+    public void main(String[] args) throws IOException {
+
+    }
 
 }
-
-
-
-
